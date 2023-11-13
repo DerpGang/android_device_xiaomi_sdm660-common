b00d98cc919bb9ed771332a31000a1e3fd90083e
Bruno Martins <bgcngm@gmail.com>
2023-11-13 13:48:00 +0000

    sdm660-common: Move to QTI health AIDL service
    The health AIDL HAL service provides functionalities of charger,
    therefore system charger at /system/bin/charger is deprecated.
    On top of that, QTI health AIDL HAL service enables suspend by
    default, the equivalent of setting ro.charger.enable_suspend
    for legacy charger.
    
    Change-Id: I59c23e7974cea1174b0161f31a535fa3afa1e5c9
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -215,8 +215,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 ifneq ($(AB_OTA_UPDATER),true)
 PRODUCT_PACKAGES += \
