fb159213b13be562ed2ee24b9333987bc31c0a9c
Vaisakh Murali <vaisakhmurali@gmail.com>
2024-06-12 18:13:42 +0530

    sdm660-common: Build vendor variant of libpng
    Change-Id: I41ba72e851355dd8bce94cde1d3ce8d685078813
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -126,6 +126,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.5:64 \
     android.hardware.camera.provider@2.6:64 \
     libdng_sdk.vendor \
+    libpng.vendor \
     vendor.qti.hardware.camera.device@1.0:64
 
 # Configstore
