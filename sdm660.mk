9c75c815c60e06e994ea6f884cb5baf9abd613c9
Michael Bestas <mkbestas@lineageos.org>
2024-10-12 20:24:21 +0800

    sdm660-common: Switch to AIDL USB GADGET hal
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624 | AOSP
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -559,7 +559,7 @@ AB_OTA_UPDATER := false
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
