93f2e79716ae103ac252fdc4cc77a85f18cfbf0f
Michael Bestas <mkbestas@lineageos.org>
2025-01-21 11:23:14 +0000

    sdm660-common: Shim libwvhidl with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -4,14 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib64/libwvhidl.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'sdm660-common',
     'xiaomi',
+    blob_fixups=blob_fixups,
     check_elf=False,
 )
 
