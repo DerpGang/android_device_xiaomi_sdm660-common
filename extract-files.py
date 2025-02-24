d64a1579e7b5586f96d0d1210ccde30d8a94b9c4
Aaron Kling <webgeek1234@gmail.com>
2025-02-24 21:33:46 +0000

    sdm660-common: enable ELF checks
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -8,10 +8,36 @@ from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
 )
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+
+namespace_imports = [
+    'device/xiaomi/sdm660-common',
+    'hardware/qcom-caf/msm8998',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um',
+    'vendor/qcom/opensource/dataservices',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+    ): lib_fixup_vendor_suffix,
+    (
+    ): lib_fixup_remove,
+}
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libwvhidl.so': blob_fixup()
@@ -22,7 +48,8 @@ module = ExtractUtilsModule(
     'sdm660-common',
     'xiaomi',
     blob_fixups=blob_fixups,
-    check_elf=False,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
